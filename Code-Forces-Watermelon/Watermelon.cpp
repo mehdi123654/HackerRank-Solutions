4dfe1e91efe43952aced09057c09b04a02fa3cfa
mehdi <mehdi.fathallah@esprit.tn>
2022-01-23 04:53:49 +0100

    Watermelon from Code Forces

---
+++ Code-Forces-Watermelon/Watermelon.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    int w;
+
+    // do
+    // {
+    cin >> w;
+    try
+    {
+        
+        if  (w < 100)
+        {
+            if (w % 2 == 0)
+            {
+                cout << "YES";
+            }
+            else
+            {
+                cout << "NO";
+            }
+        }
+        else
+        {
+            throw("entier hors domaine!");
+        }
+    }
+    catch (char const *ch)
+    {
+        cerr << ch << endl;
+    }
+    //} while (w < 1 || w > 100);
+    return 0;
+}
